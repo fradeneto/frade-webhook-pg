ca0c89df2fbd48d9ea118b6576da48bd6b4a938f
Orlando Frade <fradeneto@gmail.com>
2020-04-03 17:51:06 -0300

    feat: test webhook

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const app = require('./app');
 
 const port = process.env.PORT || 0;
+
 http.createServer(app).listen(port, () => {
   console.log(
     `Express server listening on port ${port} | NODE_ENV=${process.env.NODE_ENV}`
